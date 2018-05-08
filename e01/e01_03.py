f7ecd024a1cbb281b3644e399744bc3e8e3d6e4b
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-08 20:36:53 +0900

    e01_03 解答しました。

---
+++ e01/e01_03.py
@@ -0,0 +1,14 @@
+# 復習課題 1-3
+# インタープリタをしばらくいじってみよう。電卓のように使うために、8 * 9と入力するしていただきたい。
+# Enterキーを押して結果を見よう。Pythonは72と表示するはずだ。
+
+# 解答：
+"""
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 8 * 9
+72
+>>> 
+"""
