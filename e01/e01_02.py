eed9f49336f6a9c2fc3bc63f331119c503e7c143
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-08 20:36:33 +0900

    e01_02 解答しました。

---
+++ e01/e01_02.py
@@ -0,0 +1,12 @@
+# 復習課題 1-2
+# この章で説明したPython3の対話型インタープリタを起動しよう。
+
+# 解答：
+# 以下にインタプリタ起動画面を示します。
+
+"""
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 
+"""
