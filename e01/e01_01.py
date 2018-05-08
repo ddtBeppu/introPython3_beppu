2dadaee0083051ab10e8a1105709c9f3f4838ffb
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-08 20:35:43 +0900

    e01_01  解答しました。

---
+++ e01/e01_01.py
@@ -0,0 +1,11 @@
+# 復習課題 1-1
+# まだ自分のコンピュータにPython3をインストールしていない場合は、今ここでインストールしよう。
+
+# 解答：
+# インストール済みですので、以下の通り、インタプリタ起動画面を示します。
+
+
+# Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+# [GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 
