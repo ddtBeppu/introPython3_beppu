ad89b5dc5bd457a5c242cdd0fdfd05d6c8f87d2a
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-08 20:37:12 +0900

    e01_04  解答しました。

---
+++ e01/e01_04.py
@@ -0,0 +1,13 @@
+# 復習課題 1-4
+# 47という数値を入力してEnterキーを押そう。次の行に47と表示されただろうか？
+
+# 解答：
+"""
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 47
+47
+>>> 
+"""
