fbd87e979ecb1e37a19bbad0c4d67de2739d8136
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 20:13:08 +0900

    e03_05   題意に沿うように修正しました。

--- e03/e03_05.py
+++ e03/e03_05.py
@@ -12,8 +12,9 @@ Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33)
 [GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
 Type "help", "copyright", "credits" or "license" for more information.
 >>> things = ["mozzarella", "cinderella", "salmonella"]  # リストthingsを作成
->>> things[1].capitalize()  ### 人間を表す"conderella"の先頭文字を大文字にする
-'Cinderella'
+>>> things[1] = things[1].capitalize()   ###  人間を表す"conderella"の先頭文字を大文字にして、リストを更新
+>>> things   ###  リストを表示
+['mozzarella', 'Cinderella', 'salmonella']
 >>> 
 
 """
