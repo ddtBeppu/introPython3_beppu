e5f4766c286b1a827e1d77ace47453c320e1bb21
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 20:22:17 +0900

    e03_08  解答しました。

---
+++ e03/e03_08.py
@@ -0,0 +1,18 @@
+# 復習課題 3-8
+# "Groucho", "Chico", "Harpo"を要素としてsurpriseというリストを作ろう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> surprise = ["Groucho", "Chico", "Harpo"]   ### リストsurpriseを作成
+>>> 
+
+
+"""
