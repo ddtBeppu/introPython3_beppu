658d65e79f7cff2861f303f4801223f97123b66c
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 20:04:02 +0900

    e03_04  解答しました。

---
+++ e03/e03_04.py
@@ -0,0 +1,17 @@
+# 復習課題 3-4
+# "mozzarerra", "cinderella", "salmonella"の3つの文字列を要素としてthingsというリストを作ろう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> things = ["mozzarella", "cinderella", "salmonella"]  ### リストthingsを作成
+>>> 
+
+"""
