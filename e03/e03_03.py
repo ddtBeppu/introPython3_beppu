b073d9696a99d2e07d78e102d570b5b1f44a9299
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 20:00:06 +0900

    e03_03.py

---
+++ e03/e03_03.py
@@ -0,0 +1,19 @@
+# 復習課題 3-3
+# years_listに含まれている年の中で、あなたがもっとも年長だった年はどれか。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> years_list = [1993, 1994, 1995, 1996, 1997, 1998]  # リストの作成
+>>> years_list[-1]  ### リストの末尾の要素を指定することで、年長の年を表示
+1998
+>>> 
+
+"""
