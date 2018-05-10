b21febb8fcfbe335285dc9d7a7f40db75a9b044b
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 19:50:20 +0900

    e03_01  解答しました。

---
+++ e03/e03_01.py
@@ -0,0 +1,17 @@
+# 復習課題 3-1
+# 誕生年から５歳の誕生日を迎える年までの各年を順に並べてyears_listというリストを作ろう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> years_list = [1993, 1994, 1995, 1996, 1997, 1998]  ### リストの作成
+>>> 
+
+"""
