9ee8356059b6ec724123b0cee4543ee3e091b1d7
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 19:54:47 +0900

    e03_02  解答しました。

---
+++ e03/e03_02.py
@@ -0,0 +1,19 @@
+# 復習課題 3-2
+# years_listの要素で３歳の誕生日を迎えた年はどれか。最初の年は０歳だということを忘れないように。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> years_list = [1993, 1994, 1995, 1996, 1997, 1998]  # リストの作成
+>>> years_list[3]   ### ３歳の誕生日を迎えた歳を表す要素を表示
+1996
+>>> 
+
+"""
