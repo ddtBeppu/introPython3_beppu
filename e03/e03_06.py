7f16c3aa3e8064ad17d68d7c2dd2873eca2ee4fa
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-10 20:15:53 +0900

    e03_06  解答しました。

---
+++ e03/e03_06.py
@@ -0,0 +1,23 @@
+# 復習課題 3-6
+# thingsの中でチーズの要素をすべて大文字にして、リストを表示しよう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> things = ["mozzarella", "cinderella", "salmonella"]  # リストthingsを作成
+>>> things[1] = things[1].capitalize()   #  人間を表す"conderella"の先頭文字を大文字にして、リストを更新
+>>> things   #  リストを表示
+['mozzarella', 'Cinderella', 'salmonella']
+>>> things[0] = things[0].upper()   ### チーズを表す"mozzarella"の値を全て大文字にして、リストを更新
+>>> things   ###  リストを表示
+['MOZZARELLA', 'Cinderella', 'salmonella']
+>>> 
+
+"""
