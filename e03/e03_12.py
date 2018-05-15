04ff99a15a19b18f49e034c821c2d99d6375291e
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-15 19:50:52 +0900

    e03_12  解答しました。

---
+++ e03/e03_12.py
@@ -0,0 +1,31 @@
+# 復習課題 3-12
+# e2fからf2eという仏英辞書を作ろう。itemsメソッドを使うこと。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> e2f = {
+...     "dog": "chien",  # 英/dog: 仏/chien
+...     "cat": "chat",   # 英/cat: 仏/chat
+...     "walrus": "morse",   # 英/walrus: 仏/morse
+... }
+>>> e2f   # 辞書の表示
+{'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
+>>> f2e = {}   ### 空の辞書f2eを作成
+###  辞書e2fの内容を順に参照しながら、辞書を更新
+###  nameは英語、keyはフランス語
+>>> for name, key in e2f.items():
+...     f2e.update({key: name})  ###  新しい辞書f2eの内容に、英/仏が逆順の状態で代入
+... 
+>>> f2e   ###   仏英辞書になっていることを確認
+{'chien': 'dog', 'chat': 'cat', 'morse': 'walrus'}
+>>> 
+
+"""
