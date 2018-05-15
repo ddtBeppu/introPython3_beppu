6a31fa5a7bca05dc5b6f2dcdab077d1255b6c916
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-15 21:03:10 +0900

    e03_16  解答しました。

---
+++ e03/e03_16.py
@@ -0,0 +1,28 @@
+# 復習課題 3-16
+# lifeの最上位のキーを表示しよう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> cats = ['Henri', 'Grumpy', 'Lucy']   #  リストを作成
+>>> octopi = {}   #  空集合を作成
+>>> emus = {}   #  空集合を作成
+>>> animals = {'cats': cats, 'octopi': octopi, 'emus': emus}   #  上記のリスト、空集合を値に持つ辞書を作成
+>>> plants = {}   #  空集合を作成
+>>> other = {}   #  空集合を作成
+>>> Life = {'animals': animals, 'plants': plants, 'other': other}   # 辞書を値に持つ辞書を作成
+>>> Life    #  辞書の内容を表示
+{'animals': {'cats': ['Henri', 'Grumpy', 'Lucy'], 'octopi': {}, 'emus': {}}, 'plants': {}, 'other': {}}
+>>> Life.keys()   ###  最上位のキーを表示
+dict_keys(['animals', 'plants', 'other'])
+>>> 
+
+
+"""
