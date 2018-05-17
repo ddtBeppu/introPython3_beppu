bbd009b55d3e63d60d4109b21a225e1f5f293544
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-17 20:08:29 +0900

    e04_08   解答しました。

---
+++ e04/e04_08.py
@@ -0,0 +1,26 @@
+# 復習課題 4-8
+# ['Harry', 'Ron', 'Harmione']というリストを返すgoodという関数を定義しよう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+###  関数good()を定義
+>>> def good():
+        ### 以下の文字列を返す
+...     return ['Harry', 'Ron', 'Harmione']
+... 
+### 関数good()を実行
+>>> good()
+['Harry', 'Ron', 'Harmione']
+>>> 
+
+
+"""
