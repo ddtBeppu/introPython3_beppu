c10951a28e0aca05fcb3d519b5f0db40ec548b4e
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-17 19:42:10 +0900

    e04_04  題意に沿っていなかったため、修正しました。

--- e04/e04_04.py
+++ e04/e04_04.py
@@ -12,13 +12,10 @@ NaotonoMacBook-puro:~ naotobeppu$ python3
 Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
 [GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
 Type "help", "copyright", "credits" or "license" for more information.
->>> list = []   ###  空のリストを作成。値を追加していく。
-###  0から9までの値で、偶数のみを参照しながらループ
->>> for i in range(0, 10, 2):
-        iの値を追加
-...     list.append(i)
-... 
->>> list   ###  listの値を表示
+### リスト内包表記により、偶数リストを作成
+>>> list = [number for number in range(10) if number%2 == 0]
+###  リストの値を表示
+>>> list
 [0, 2, 4, 6, 8]
 >>> 
 
