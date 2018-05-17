b6e17c8f2508bad42e44124032ab71a849ac71c4
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-17 19:21:50 +0900

    e04_03  解答しました。

---
+++ e04/e04_03.py
@@ -0,0 +1,27 @@
+# 復習課題 4-3
+# forループを使ってリスト[3, 2, 1, 0]の値を表示しよう。
+
+# 解答：
+# インタープリタにて計算を行いました。
+# コメントは、後からエディタにて追加しました。
+
+"""
+
+NaotonoMacBook-puro:~ naotobeppu$ python3
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> for num in [3, 2, 1, 0]:   ###  for文のループ条件にリストを加えて、ループ
+        ### リストの値を順に表示
+...     print(num)
+... 
+
+### 実行結果
+3
+2
+1
+0
+>>> 
+
+
+"""
