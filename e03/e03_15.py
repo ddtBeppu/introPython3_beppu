dd86fd90bc52ccd19d50dbcca24ea79c131518e7
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-17 19:15:46 +0900

    e03_15  修正しました。

--- e03/e03_15.py
+++ e03/e03_15.py
@@ -13,14 +13,14 @@ NaotonoMacBook-puro:~ naotobeppu$ python3
 Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 12:04:33) 
 [GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
 Type "help", "copyright", "credits" or "license" for more information.
->>> cats = ['Henri', 'Grumpy', 'Lucy']
->>> octopi = {}
->>> emus = {}
->>> animals = {'cats': cats, 'octopi': octopi, 'emus': emus}
->>> plants = {}
->>> other = {}
->>> Life = {'animals': animals, 'plants': plants, 'other': other}
->>> Life
+>>> cats = ['Henri', 'Grumpy', 'Lucy']   ###  リストを作成
+>>> octopi = {}   ###  空集合を作成
+>>> emus = {}   ###  空集合を作成
+>>> animals = {'cats': cats, 'octopi': octopi, 'emus': emus}   ###  上記のリスト、空集合を値に持つ辞書を作成
+>>> plants = {}   ###  空集合を作成
+>>> other = {}   ###  空集合を作成
+>>> Life = {'animals': animals, 'plants': plants, 'other': other}   ### 辞書を値に持つ辞書を作成
+>>> Life    ###  辞書の内容を表示
 {'animals': {'cats': ['Henri', 'Grumpy', 'Lucy'], 'octopi': {}, 'emus': {}}, 'plants': {}, 'other': {}}
 >>> 
 
