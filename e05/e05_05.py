7f840501488c553c71805898aae7ebc40965976b
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-22 20:32:55 +0900

    e05_05  解答しました。

---
+++ e05/e05_05.py
@@ -0,0 +1,16 @@
+# 復習課題 5-5
+# 'a': 1, 'b': 2, 'c': 3というキー/値ペアを使ってplainという辞書を作り、内容を表示しよう。
+
+# 辞書の作成
+plain = {'a': 1, 'b': 2, 'c': 3}
+### 値の表示
+print("plain = ", plain)
+
+
+# 実行結果
+"""
+
+NaotonoMacBook-puro:e05 naotobeppu$ python e05_05.py 
+plain =  {'a': 1, 'b': 2, 'c': 3}
+
+"""
