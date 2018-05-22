eeb2893acece132d912d48527cb6f173a41bd6cb
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-22 21:12:39 +0900

    e06_02  解答しました。

---
+++ e06/e06_02.py
@@ -0,0 +1,19 @@
+# 復習課題 6-2
+# Thing2という新しいクラスを作り、lettersというクラス属性に'abc'という値を代入して、lettersを表示しよう。
+
+# クラスThing2()を作成
+class Thing2():
+    # クラス属性に値を代入
+    letters = 'abc'
+
+# クラス属性の値を表示
+print(Thing2.letters)
+
+
+# 実行結果
+"""
+
+NaotonoMacBook-puro:e06 naotobeppu$ python e06_02.py 
+abc
+
+"""
