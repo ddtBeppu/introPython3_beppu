ba1e8408b1e82fb448b92e0b188de8d090744a60
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-24 19:58:43 +0900

    e06_04  解答しました。

---
+++ e06/e06_04.py
@@ -0,0 +1,14 @@
+# 復習課題 6-4
+# name, symbol, numberというインスタンス属性を持つElementというクラスを作り、
+# 'Hydrogen', 'H', 1という値を持つこのクラスのオブジェクトを作ろう。
+
+# クラスElementを作成
+class Element():
+    #  初期化メソッドを定義
+    def __init__(self, name, symbol, number):
+        self.name = name # 属性の追加
+        self.symbol = symbol　# 属性の追加
+        self.number = number　# 属性の追加
+
+# オブジェクトの作成
+element = Element('Hydrogen', 'H', 1)
