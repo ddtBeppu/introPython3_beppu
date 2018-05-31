a54d2f24cc3dcc505584343d7834cdc71d25975f
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-31 19:57:30 +0900

    e07_05  解答しました。

---
+++ e07/e07_05.py
@@ -0,0 +1,18 @@
+# 復習課題 7-5
+# 新しいスタイルの書式指定を使って定型書簡を作りたい。次の文字列をletterという変数に保存しよう(次の問題で使う)。
+
+"Dear {salutation} {name},\
+\
+Thank you for your letter. We are sorry that our {product} {verbed} in your\
+{room}. Please note that it should never be used in a {room}, especially\
+near any {animals}.\
+\
+Send us your receipt and {amount} for shipping and handling. We will send\
+you another {product} that, in our tests, is {percent}% less likely to\
+have {verved}.\
+\
+Thank you for your support.\
+\
+Sincerely,\
+{spokesman}\
+{job_title}"
