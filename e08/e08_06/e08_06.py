43bde393cf633f429721f71409b9cfeda2630284
Naoto Beppu <ddt.naoto@gmail.com>
2018-06-26 19:52:43 +0900

    e08_06 不要なコメントを削除しました。

--- e08/e08_06/e08_06.py
+++ e08/e08_06/e08_06.py
@@ -11,7 +11,7 @@ cursor = conn.cursor()
 
 # dbにテーブルbookを作成するsql文を定義
 insert_sql = 'CREATE TABLE book (title VARCHAR(20), author VARCHAR(20), year INT)'
-#insert_sql = 'DELETE TABLE book'
+
 # 実行する
 cursor.execute(insert_sql)
 
