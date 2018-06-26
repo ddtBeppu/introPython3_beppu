c36fb311cc8297c27297d08b42f3106968d07dbb
Naoto Beppu <ddt.naoto@gmail.com>
2018-06-26 20:34:12 +0900

    e08_05  題意に沿うように解き直しました。

--- e08/e08_05/e08_05.py
+++ e08/e08_05/e08_05.py
@@ -21,15 +21,8 @@ books = [
     ['Small Gods', 'Terry Pratchett', '1992'],
 ]
 
-# 前のbooksファイルを開き、内容を読み取る
-with open('../e08_03/books', 'rt') as fin:
-    # forループで内容を抽出できる行を作成
-    cin = csv.reader(fin)
-    # 内容を読み出す
-    books_pre = [row for row in cin]
-    # 書き出しを行う
-    with open('books', 'wt') as fout2:
-        # 書き込む対象を設定
-        csvout = csv.writer(fout2)
-        # e08_03で作成した内容の次の行から今回のbooksの内容を表示
-        csvout.writerows(books_pre + books)
+with open('books', 'wt') as fout2:
+    # 書き込む対象を設定
+    csvout = csv.writer(fout2)
+    # e08_03で作成した内容の次の行から今回のbooksの内容を表示
+    csvout.writerows(books)
