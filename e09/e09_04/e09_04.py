e4893a3249a5fc96967eddc7655840c877a96358
Naoto Beppu <ddt.naoto@gmail.com>
2018-07-02 23:28:03 +0900

    e09_04 解答しました。

---
+++ e09/e09_04/e09_04.py
@@ -0,0 +1,13 @@
+# 復習課題 9-4
+# 次のような内容でhome.htmlという名前のJinja2テンプレートファイルを作ろう。
+"""
+<html>
+<head>
+<title>It's alive!</title>
+<body>
+I'm of course referring to {{thing}}, which is {{height}} feet tall and {{color}}.
+</body>
+</html>
+"""
+
+# home.html作成済み
